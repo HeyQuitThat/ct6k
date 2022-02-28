45fc619ebb8b42b0b725af70b22cd8cdab953cd5
Mitch Williams <cluemerchant@gmail.com>
2022-02-27 17:38:56 -0800

    ct6k: add GPL headers
    Add GPL headers to each source file.
    
    Signed-off-by: Mitch Williams <cluemerchant@gmail.com>

--- src/memory.hpp
+++ src/memory.hpp
@@ -1,3 +1,20 @@
+/*
+    The Comp-o-Tron 6000 software is Copyright (C) 2022 Mitch Williams.
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License version 2 as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
 // memory.hpp - Prototypes and declarations for memory class
 #ifndef __MEMORY_HPP__
 #define __MEMORY_HPP__
