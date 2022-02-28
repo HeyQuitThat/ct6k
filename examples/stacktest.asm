efbceebd8e4a068b3580248b1c1fc6a7d5e69883
Mitch Williams <cluemerchant@gmail.com>
2022-02-27 16:14:19 -0800

    ct6k examples: extend test code
    Add a few new instructions to stacktest.asm in order to get full
    coverage of instructions tested.
    
    Signed-off-by: Mitch Williams <cluemerchant@gmail.com>

--- examples/stacktest.asm
+++ examples/stacktest.asm
@@ -1,6 +1,8 @@
 * stacktest.asm - ct6k stack validation, with some fun math thrown in
     MOVE 0xF0F0F0F0, R0
     MOVE 0x1000, R14  * stack pointer
+    MOVE 0x2000, R10
+    SETIHAP R10
     MOVE I0, R3
     INTENA
     PUSH R0
@@ -28,4 +30,15 @@
     MOVE 0x3, R3
     SHIFTR R1, R3, R4
     INTDIS
+    MOVE 0x10, R0
+    MOVE 0x11, R1
+    CMP R1, R0
+    JOVER $BEYOND * should jump
+$END
+    HALT
+
+$BEYOND
+    CMP R0, R0
+    JNUNDER $END  * should jump
+    NOP
     HALT
