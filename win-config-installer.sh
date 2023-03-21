96e9b95c733a3254f3aa3271f5260b7d61e9aaff
Mitch Williams <cluemerchant@gmail.com>
2023-03-21 10:22:38 -0700

    ct6k install: tidy up Windows install scripts
    Modify installer .exe filename to include version number.
    Add comments to indicate why there are bash scripts for building Windows
    drivers. With the alternative being PowerShell, bash looks pretty good
    from this angle.
    
    Signed-off-by: Mitch Williams <cluemerchant@gmail.com>

--- win-config-installer.sh
+++ win-config-installer.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+# This script is designed to be run from the Cygwin environemnt on Windows
+# using the Qt 6.4.2 toolchain and the Qt Installer Frameword version
+# 4.5.1. Yes, the paths are hard-coded. Yes, they should be fixed.
+MAJOR=$(grep VERSION_MAJOR ct6k-gui/ct6k-info.h | cut -f 2 -d "\"")
+MINOR=$(grep VERSION_MINOR ct6k-gui/ct6k-info.h | cut -f 2 -d "\"")
+SUBMINOR=$(grep VERSION_SUB ct6k-gui/ct6k-info.h | cut -f 2 -d "\"")
+REV=$(grep VERSION_EXTRA ct6k-gui/ct6k-info.h | cut -f 2 -d "\"")
+if [ -z ${REV} ]; then
+	REV=1
+fi
+WINNAME="ct6k-install-$MAJOR.$MINOR.$SUBMINOR.exe"
 mkdir pkg/packages/org.ct6k.ct6k/data
 cp *Release/*.exe pkg/packages/org.ct6k.ct6k/data
 pushd pkg/packages/org.ct6k.ct6k/data
@@ -13,4 +24,4 @@ cp pkg/config/*.ico pkg/packages/org.ct6k.ct6k/data
 cp COPYING pkg/packages/org.ct6k.ct6k/meta/
 cp OFL.txt pkg/packages/org.ct6k.ct6k/meta/
 cd pkg
-/cygdrive/c/Qt/QtIFW-4.5.1/bin/binarycreator.exe --offline-only -c config/config.xml -p packages/ ct6k-gui-install.exe
+/cygdrive/c/Qt/QtIFW-4.5.1/bin/binarycreator.exe --offline-only -c config/config.xml -p packages/ ${WINNAME}
--- win-install-clean.sh
+++ win-install-clean.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# This script is designed to be run from the Cygwin environemnt on Windows.
 rm -f pkg/config/*ico
 rm -f pkg/*.exe
 rm -rf pkg/packages/org.ct6k.ct6k/data
